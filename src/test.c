195cfad7a64b70674a94a60b974a02f2a7a8f5ec
janmarxen <jan.marxen@gmail.com>
2024-11-27 15:01:29 +0100

    Change of computers

--- src/test.c
+++ src/test.c
@@ -57,4 +57,30 @@ double test_naive_matmat(const size_t N, const double eps) {
 	free(C);
 	free(C_gt);
 }
+double test_strassen_matmat(i, 0.001)){
+	const size_t n = pow(2, N);
+	const size_t m = n + 1;
+	const size_t k = n - 1;
+	double *A = malloc(m * n * sizeof(double));
+	double *B = malloc(n * k * sizeof(double));
+	double *C = malloc(m * k * sizeof(double));
+	double *C_gt = malloc(m * k * sizeof(double));
+	gen_rand_matrix(A, m, n);
+	gen_rand_matrix(B, n, k);
+	clock_t start = clock();  // Record start time
+	naive_matmat(A, B, C, m, n, k);
+	clock_t end = clock();	// Record end time
+	double time_spent =
+	    (double)(end - start) / CLOCKS_PER_SEC;  // Calculate elapsed time
+	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, k, n, 1., A,
+		    n, B, k, 0., C_gt, k);
+	if (compare_mat(C, C_gt, m, k, eps))
+		return time_spent;
+	else
+		return -1.0;
+	free(A);
+	free(B);
+	free(C);
+	free(C_gt);
+};
 
