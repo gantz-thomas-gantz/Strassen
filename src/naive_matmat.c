a986eac3ec81bc86dd3cee4d1a8d2d4832b98fb3
janmarxen <jan.marxen@gmail.com>
2024-12-12 17:28:53 +0100

    Small docs correction.

--- src/naive_matmat.c
+++ src/naive_matmat.c
@@ -5,23 +5,15 @@
 
 #include <stddef.h>  // for size_t
 
-/*
- * Description:
- * Multiply A (size mxn) with B (size nxk) naively, store the result in C (size
- * mxk).
- *
- * Matrix format:
- * Matrices should be flattened arrays in row-major format.
- */
 void naive_matmat(double *A, double *B, double *C, const size_t m,
-		  const size_t n, const size_t k){
-    for(size_t i=0; i<m; i++){
-        for(size_t j=0; j<k; j++){
-            C[i*k+j] = 0;
-            for(size_t l=0; l<n; l++){
-                C[i*k+j] += A[i*n+l]*B[l*k+j];
-            }
-        }
-    }
+		  const size_t n, const size_t k) {
+	for (size_t i = 0; i < m; i++) {
+		for (size_t j = 0; j < k; j++) {
+			C[i * k + j] = 0;
+			for (size_t l = 0; l < n; l++) {
+				C[i * k + j] += A[i * n + l] * B[l * k + j];
+			}
+		}
+	}
 }
 
